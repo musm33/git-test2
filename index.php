2810b51cafe9e7117d30fd728b2bce893d60f5d3
musm33 <musmuliadi33308@gmail.com>
2024-06-06 22:20:04 +0800

    + paragraf pada index.html

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
    <H1>hello world!</H1> 
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ipsum blanditiis fuga deleniti nobis. Nobis sit aliquam ullam, amet facilis labore provident distinctio minus at doloremque praesentium est, dignissimos aut?</p>
 </body>
 </html>
